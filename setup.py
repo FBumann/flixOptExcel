eb870f8e01258138509c6335c2c4bd142b29cc59
FBumann <117816358+FBumann@users.noreply.github.com>
2024-01-03 16:17:46 +0100

    Added flixOpt to the requirements

--- setup.py
+++ setup.py
@@ -12,6 +12,5 @@ setup(
     package_data={
         'flixOptExcel': ['flixOpt_excel/resources/ExcelTemplates/*.xlsx'],
     },
-    #install_requires=read_requirements('requirements.txt')
-    install_requires=['matplotlib==3.8.2', 'networkx==3.2.1', 'openpyxl==3.1.2', 'plotly==5.18.0', 'pprintpp==0.4.0', 'git+https://github.com/FBumann/flixOpt.git']
+    install_requires=read_requirements('requirements.txt')
 )
