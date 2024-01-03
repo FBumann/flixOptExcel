704d9d2e6dafc41199f8c0faa51c114d48d6d63a
FBumann <117816358+FBumann@users.noreply.github.com>
2024-01-03 13:57:56 +0100

    Added excel files

--- setup.py
+++ setup.py
@@ -8,5 +8,6 @@ setup(
     name='flixOptExcel',
     version='0.1.0',
     packages=find_packages(),
+    include_package_data=True,  # Include non-code files specified in MANIFEST.in
     install_requires=read_requirements('requirements.txt')
 )
