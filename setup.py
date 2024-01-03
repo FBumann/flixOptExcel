dd45a2c92a20f2067054c82301ce37e5064c64d8
FBumann <117816358+FBumann@users.noreply.github.com>
2024-01-03 14:01:58 +0100

    Added excel files

--- setup.py
+++ setup.py
@@ -9,5 +9,8 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     include_package_data=True,  # Include non-code files specified in MANIFEST.in
+    package_data={
+        'flixOptExcel': ['flixOpt_excel/resources/*.xlsx','flixOpt_excel/resources/ExcelTemplates/*.xlsx'],
+    },
     install_requires=read_requirements('requirements.txt')
 )
