ba67b220e070569cb7c4fa101c0f1dc2e37b92fb
FBumann <117816358+FBumann@users.noreply.github.com>
2024-02-07 18:57:57 +0100

    Add metadata and modify dependencies in setup.py
    The setup script has been updated with author details, description, classifiers, and project url. The version constraints for dependencies in requirements.txt have also been relaxed to accept newer versions. Plus, a minor adjustment is made in .gitignore file.

--- setup.py
+++ setup.py
@@ -7,6 +7,22 @@ def read_requirements(file_path):
 setup(
     name='flixOptExcel',
     version='0.1.0',
+    author='Felix Bumann',
+    author_email='felixbumann387@gmail.com',
+    description='Vector based energy and material flow optimization framework in python.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/FBumann/flixOptExcel',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        'Topic :: Scientific/Engineering :: Mathematics :: Numerical Analysis :: Optimization',
+        'Topic :: Software Development :: Libraries :: Python'
+    ],
     packages=find_packages(),
     include_package_data=True,  # Include non-code files specified in MANIFEST.in
     package_data={
