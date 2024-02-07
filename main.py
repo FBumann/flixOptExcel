263e5b573119d1170366cd3ed40f1e5605502641
FBumann <117816358+FBumann@users.noreply.github.com>
2024-02-07 19:48:30 +0100

    Main function refactored to use the new class and function based structure

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-from flixOpt_excel.Initiation.Excel_Modell import run_excel_model
-
-
+from flixOpt_excel.Initiation.Modules import ExcelModel
 
 
 
@@ -12,6 +10,13 @@ solver_name = "cbc"         #  open source solver
 #solver_name = "gurobi"     # commercial solver (Free academic licences). Much faster for large Models and storages
 
 
+def main():
+    excel_model = ExcelModel(excel_file_path=excel_file_path)
+    excel_model.solve_model(solver_name=solver_name, gap_frac=0.01, timelimit=3600)
+    excel_model.visualize_results()
+    # calculation_results_for_further_inspection = excel_model.load_results()
+
+
 
 
 
@@ -21,12 +26,13 @@ solver_name = "cbc"         #  open source solver
 
 
 if __name__ == '__main__':
-    run_excel_model(excel_file_path, solver_name, gap_frac=0.001, timelimit= 3600)
+    main()
 
 # optional: change values for gap_frac and timelimit
 '''
 :param gap_frac:
     0...1 ; gap to relaxed solution. Higher values for faster solving. 0...1
 :param timelimit:
-    timelimit in seconds. After this time limit is exceeded, the solution process is stopped
+    timelimit in seconds. After this time limit is exceeded, the solution process is stopped and the best yet found result is used. 
+    If no result is found yet, There Process is aborted
 '''
