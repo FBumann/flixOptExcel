dc50b28a5204208b1250b7806024493e9afcfffc
FBumann <117816358+FBumann@users.noreply.github.com>
2024-02-09 17:44:32 +0100

    Add new run_test.py file and update .gitignore
    A new test script called run_test.py has been created which runs tests using the ExcelModel class from the flixOptExcel module. Also, the .gitignore file has been updated to ignore test results instead of testing folder.

---
+++ tests/run_test.py
@@ -0,0 +1,38 @@
+from flixOptExcel.Initiation.Modules import ExcelModel
+import os
+
+
+name_of_excel_sheet = "test_minimal.xlsx"
+excel_file_path = os.path.join(os.getcwd(), name_of_excel_sheet)
+
+
+solver_name = "cbc"         #  open source solver
+#solver_name = "gurobi"     # commercial solver (Free academic licences). Much faster for large Models and storages
+
+
+def main():
+    excel_model = ExcelModel(excel_file_path=excel_file_path)
+    excel_model.solve_model(solver_name=solver_name, gap_frac=0.01, timelimit=3600)
+    excel_model.visualize_results()
+    # calculation_results_for_further_inspection = excel_model.load_results()
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
+
+# optional: change values for gap_frac and timelimit
+'''
+:param gap_frac:
+    0...1 ; gap to relaxed solution. Higher values for faster solving. 0...1
+:param timelimit:
+    timelimit in seconds. After this time limit is exceeded, the solution process is stopped and the best yet found result is used. 
+    If no result is found yet, There Process is aborted
+'''
